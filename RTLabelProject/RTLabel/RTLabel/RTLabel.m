19206113a68e19f965dd9829221f67917f8cc094
Piotr Adamczak <piotr.adamczak@appunite.com>
2012-06-20 15:08:16 +0200

    RTLabel StaticLibrary

---
+++ RTLabelProject/RTLabel/RTLabel/RTLabel.m
@@ -0,0 +1,23 @@
+//
+//  RTLabel.m
+//  RTLabel
+//
+//  Created by Piotr Macbook on 6/20/12.
+//  Copyright (c) 2012 UNI. All rights reserved.
+//
+
+#import "RTLabel.h"
+
+@implementation RTLabel
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
